0b63f491269ea88db3a46736a8fa3e9f4b9a192c
Mayank Raj <mayankrj1224@gmail.com>
2023-02-15 21:27:43 +0530

    assignment

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routers = require('./Router/userRoutes')
+const route= express.Router();
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/',routers),
-
+route.get('/', (req,res)=>{
+    res.send("Server is Live")
+})
 port = process.env.PORT || 3030;
 mongoose.set("strictQuery",true);
 mongoose.connect(process.env.DATA_URl)
